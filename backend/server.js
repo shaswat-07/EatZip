3d16be1cd3c1cbcfc8eb5ef06750708976fd494a
Shaswat Pathak <shaswatpathak9508@gmail.com>
2026-06-25 11:44:43 +0530

    Update backend cors URL for production

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,11 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 
-    'http://localhost:5174'],
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://food-delivery-app-sandy-tau-21.vercel.app'
+    ],
     credentials: true
 }))
 
